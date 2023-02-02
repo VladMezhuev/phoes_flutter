73e56b9c1734db5d86dce4948cea28d6b502d28c
Vlad <vlad.mezhuev11@gmail.com>
2023-02-02 15:01:22 +0100

    delete old code in di

--- lib/di.dart
+++ lib/di.dart
@@ -1,17 +1,6 @@
 import 'package:get_it/get_it.dart';
-import 'package:phone_app/data/order_repo.dart';
-import 'package:phone_app/data/phones_repository.dart';
-import 'package:phone_app/data/shared_pref.dart';
-
 import 'package:injectable/injectable.dart';
 import 'package:phone_app/di.config.dart';
-
-//
-// void setup() {
-//   getIt.registerSingleton<PhonesRepository>(PhonesRepository());
-//   getIt.registerSingleton<OrderRepository>(OrderRepository());
-//   getIt.registerSingleton<AppPreferences>(AppPreferences());
-// }
 
 final getIt = GetIt.instance;
 
